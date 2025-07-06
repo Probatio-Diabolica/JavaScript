ba8980dc3427aa22e66f09ead47e160958857be7
Hearn <ayushkaintura1123@gmail.com>
2025-07-06 22:03:13 +0530

    first suffering

---
+++ vars.js
@@ -0,0 +1,25 @@
+class fucker {
+    constructor(str) {
+        
+    }
+
+    says(str)
+    {
+        console.log("Fuck you Webdev" + str);
+    }
+};
+
+//normal var
+let fuck = new fucker();
+
+fuck.says();
+
+//const var
+const hoe = new fucker();
+hoe.says(" rawr");
+
+var bitch = hoe;
+bitch.says(" Hiiii");
+
+
+
